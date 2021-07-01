a589202939eac7885055dca0736f9ea64eb0c2b8
Josue Huancapaza Cordova <jhuancapaza@comsatel.com.pe>
2021-07-01 14:58:15 -0500

    calse 8: ciclo de vida de react
    uso de libreria axios para consumir apis

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      usuarios: [],
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log("respuesta", respuesta);
+
+    this.setState({
       usuarios: [
         {
           nombre: "Josue",
@@ -16,9 +27,8 @@ class App extends Component {
           enlace: "platzi.com",
         },
       ],
-    };
+    });
   }
-
   ponerFilas = () =>
     this.state.usuarios.map((usuario) => (
       <tr>
