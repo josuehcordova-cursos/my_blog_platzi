03162c82590582da25cf3c53494e49f2bc1ce27c
Josue Huancapaza Cordova <jhuancapaza@comsatel.com.pe>
2021-07-01 17:11:53 -0500

    clase 12 y 1 3: Store, Reducers
    - install redux y react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Menu from "./Menu";
 import Usuarios from "./Usuarios";
-
 import { BrowserRouter, Route } from "react-router-dom";
+
 const Tareas = () => <div>Tareas</div>;
 const App = () => (
   <BrowserRouter>
     <Menu />
-    <Route exact path="/" component={Usuarios} />
-    <Route exact path="/tareas" component={Tareas} />
+    <div className="margen">
+      <Route exact path="/" component={Usuarios} />
+      <Route exact path="/tareas" component={Tareas} />
+    </div>
   </BrowserRouter>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+const store = createStore(
+  reducers, // Todos los reducers
+  {} // Estado inicial
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
