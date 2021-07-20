23cc920a3f4b4605de14ef9978681319a1f8131e
Josue Huancapaza Cordova <josuehcordova+users@gmail.com>
2021-07-19 23:50:42 -0500

    clase 23: Componente Spinner

---
+++ src/components/General/Spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../css/spinner.css";
+const Spinner = () => {
+  return (
+    <div className="center">
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
+
+import Spinner from "../General/Spinner";
 class Usuarios extends Component {
   componentDidMount() {
     this.props.traerTodos();
@@ -14,23 +16,28 @@ class Usuarios extends Component {
         <td>{usuario.website}</td>
       </tr>
     ));
+  ponerContenido = () => {
+    if (this.props.cargando) {
+      return <Spinner />;
+    }
+
+    return (
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>{this.ponerFilas()}</tbody>
+      </table>
+    );
+  };
 
   render() {
     console.log(this.props.cargando);
-    return (
-      <div>
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
-            </tr>
-          </thead>
-          <tbody>{this.ponerFilas()}</tbody>
-        </table>
-      </div>
-    );
+    return <div>{this.ponerContenido()}</div>;
   }
 }
 
